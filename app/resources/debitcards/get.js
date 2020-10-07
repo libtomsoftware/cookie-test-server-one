f45ebc249cd1ce501a73012e6f031a8f7b44256f
Tomasz Libich <t.libich@gmail.com>
2020-10-07 14:04:44 +0100

    Show console log of cookies attached to the data request

--- app/resources/debitcards/get.js
+++ app/resources/debitcards/get.js
@@ -2,6 +2,8 @@ const responder = require("../../responder");
 const CONFIG = require("../../config");
 
 module.exports = (request, response) => {
+  console.log("request cookies");
+  console.log(request.cookies);
   responder.send(
     response,
     request.get("origin"),
