f7858312bc0024e3b66ca0d2fa97a2ed2b615734
Tomasz Libich <t.libich@gmail.com>
2020-10-07 17:55:59 +0100

    Try X-frame-options

--- app/resources/debitcards/get.js
+++ app/resources/debitcards/get.js
@@ -2,8 +2,6 @@ const responder = require("../../responder");
 const CONFIG = require("../../config");
 
 module.exports = (request, response) => {
-  console.log("request cookies");
-  console.log(request.cookies);
   responder.send(
     response,
     request.get("origin"),
