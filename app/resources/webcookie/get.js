d8155000f7abf2a2bd7393abca491017d141cc8e
Tomasz Libich <t.libich@gmail.com>
2020-10-07 13:27:15 +0100

    Update cookies

--- app/resources/webcookie/get.js
+++ app/resources/webcookie/get.js
@@ -17,6 +17,7 @@ module.exports = (request, response) => {
           sameSite: "None",
           httpOnly: true,
           secure: true,
+          domain: "bertlock.net",
         };
 
         cookiesToExpire.push(cookie);
